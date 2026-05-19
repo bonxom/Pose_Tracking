8cb0994533d8bf2a3310b637f188183cc88e2100
Hq1anq <trinhhoanggiang05@gmail.com>
2026-05-19 09:47:34 +0700

    add: mock api /get_requested_enrollment

---
+++ src/constants/mocks/MOCK_REQUESTED_ENROLLMENT.js
@@ -0,0 +1,19 @@
+const MOCK_REQUESTED_ENROLLMENT = {
+  code: "1000",
+  message: "OK",
+  data: {
+    data: [
+      {
+        request: {
+          id: "ecd21e9c-abfa-45ce-b3ef-f2a46ddd82ec",
+          user_name: "Học viên 333",
+          avatar: "https://i.pravatar.cc/300",
+          created: "2026-05-16T14:50:27.502Z",
+        },
+      },
+    ],
+    total: 1,
+  },
+};
+
+export default MOCK_REQUESTED_ENROLLMENT;
