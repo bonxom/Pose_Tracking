d6cb316b9522a57cdc2aeb95a92a9df811b81437
Hq1anq <trinhhoanggiang05@gmail.com>
2026-05-27 23:36:03 +0700

    chore: clean up, remove unused layout

--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -68,8 +68,6 @@ export default function RootLayout() {
         <Stack.Screen name="(auth)" />
         <Stack.Screen name="(tabs)" />
         <Stack.Screen name="post" />
-        <Stack.Screen name="comment" />
-        <Stack.Screen name="courses/index" />
         <Stack.Screen name="settings" />
         <Stack.Screen name="chat" />
         <Stack.Screen name="profile" />
